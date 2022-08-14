9d3a6000c59e31032380b8ecce0d417b2118135e
maia <maia.celeste.camacho@gmail.com>
2022-08-14 19:10:41 -0300

    Se agrega el ejercicio 1,2 y 4 de Mysql

--- Act1-2-4.sql
+++ Act1-2-4.sql
@@ -1,4 +1,4 @@
-	-- Actividad 1
+-- Actividad 1
 CREATE TABLE Perro
    	(ID_Perro integer PRIMARY KEY NOT NULL,
    	Nombre VARCHAR(25),
@@ -7,10 +7,9 @@ CREATE TABLE Perro
         DNI_Dueno integer NOT NULL,
     	FOREIGN KEY (DNI_Dueno) REFERENCES Dueno(DNI)
    	);
-    -- Actividad 2
+    
+-- Actividad 2
 INSERT INTO Perro VALUES (6, 'MATITA', '2021-11-19', 'HEMBRA',  33222111);
 
-	-- Actividad 4
-    UPDATE Perro SET Fecha_nac = '2021-04-06' where ID_Perro = 4;
-
-
+-- Actividad 4
+UPDATE Perro SET Fecha_nac = '2021-04-06' where ID_Perro = 4;
