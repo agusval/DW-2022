65e7f7d67c8e8ce539253e49bfbab814586894c6
maia <maia.celeste.camacho@gmail.com>
2022-08-14 17:56:10 -0300

    Ejercio2 de python

--- Ejer2.py
+++ Ejer2.py
@@ -1,8 +1,9 @@
-
 #Creo lista
+
 Dueno2 =[23546987, "Maria", "Perez", 4789689, "Pueyrredon 811"]
 
 #Muestro datos, excepto DNI y APELLIDO
+
 for x in range(5):
     #Muestro datos siempre y cuando no sean de la posicion CERO y DOS de la lista
     if (x != 0) and (x != 2):
