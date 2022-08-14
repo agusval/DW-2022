4e5e0a5afb276c50360e8ae1b168928049dbb96a
Mariela Z. Lara <100329512+MARIELARA@users.noreply.github.com>
2022-08-14 17:01:38 -0300

    Update and rename dueno.py to dueno2.py

---
+++ dueno2.py
@@ -0,0 +1,8 @@
+#Creo lista
+Dueno2 = [23546987, "María", "Perez", 4789689, "Pueyrredon 811"]
+#Muestro datos exceptos DNI y APELLIDO
+for x in range (5):
+    #Muestro datos siempre y cuando no sean de la posición CERO y DOS de la lista
+    if (x!=0 ) and (x!=2):
+        print (Dueno2 [x])
+        
