8646d2c3d3f86d1260cbf29054db08d4cc3df4a8
maia <maia.celeste.camacho@gmail.com>
2022-08-14 19:18:43 -0300

    Se agregan los ejercicios 1,2 y 4 de Mysql

--- Act-UnoDosCuatro.sql
+++ Act-UnoDosCuatro.sql
@@ -1,4 +1,5 @@
-	-- Actividad 1
+-- Actividad 1
+
 CREATE TABLE Perro
    	(ID_Perro integer PRIMARY KEY NOT NULL,
    	Nombre VARCHAR(25),
@@ -7,10 +8,11 @@ CREATE TABLE Perro
         DNI_Dueno integer NOT NULL,
     	FOREIGN KEY (DNI_Dueno) REFERENCES Dueno(DNI)
    	);
-    -- Actividad 2
+
+-- Actividad 2
+
 INSERT INTO Perro VALUES (6, 'MATITA', '2021-11-19', 'HEMBRA',  33222111);
 
-	-- Actividad 4
-    UPDATE Perro SET Fecha_nac = '2021-04-06' where ID_Perro = 4;
+-- Actividad 4
 
-
+UPDATE Perro SET Fecha_nac = '2021-04-06' where ID_Perro = 4;
